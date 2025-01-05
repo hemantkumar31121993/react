d96929d632ed52d0b0b3b089b408294472d73da3
Hemant Kumar <hemantkumar311121993@gmail.com>
2025-01-05 16:12:21 +0530

    change link

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -1,24 +1,35 @@
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TreeViewPage from './tree-view';
-import {BrowserRouter, Link, Route, Routes} from "react-router";
-import TypeaheadPage from "./typeahead";
-import DragDropPage from "./drag-drop/index.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TreeViewPage from './tree-view'
+import { BrowserRouter, Link, Route, Routes } from 'react-router'
+import TypeaheadPage from './typeahead'
+import DragDropPage from './drag-drop/index.jsx'
 
 function App() {
-
     return (
         <BrowserRouter>
             <div>
-                <Link to="/tree-view" className="me-2">Tree View</Link>
-                <Link to="/typeahead" className="me-2">Typeahead</Link>
-                <Link to="/drag-n-drop" className="me-2">Drag-n-Drop</Link>
+                <Link to="/react/tree-view" className="me-2">
+                    Tree View
+                </Link>
+                <Link to="/react/typeahead" className="me-2">
+                    Typeahead
+                </Link>
+                <Link to="/react/drag-n-drop" className="me-2">
+                    Drag-n-Drop
+                </Link>
             </div>
             <div className="p-2">
                 <Routes>
-                    <Route path="/tree-view" element={<TreeViewPage/>}/>
-                    <Route path="/typeahead" element={<TypeaheadPage/>}/>
-                    <Route path="/drag-n-drop" element={<DragDropPage />} />
+                    <Route path="/react/tree-view" element={<TreeViewPage />} />
+                    <Route
+                        path="/react/typeahead"
+                        element={<TypeaheadPage />}
+                    />
+                    <Route
+                        path="/react/drag-n-drop"
+                        element={<DragDropPage />}
+                    />
                 </Routes>
             </div>
         </BrowserRouter>
