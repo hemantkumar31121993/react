4cbff5d536b1dcf9e7fcf7c0ded71bd5bd0f4ce9
Hemant Kumar <hemantkumar311121993@gmail.com>
2025-01-05 15:22:35 +0530

    update base for vite

--- components/vite.config.js
+++ components/vite.config.js
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-    include: /\.(jsx|tsx)$/,
-    babel: {
-      plugins: ['styled-components'],
-      babelrc: false,
-      configFile: false,
-    }
-  }),],
+    plugins: [
+        react({
+            include: /\.(jsx|tsx)$/,
+            babel: {
+                plugins: ['styled-components'],
+                babelrc: false,
+                configFile: false,
+            },
+        }),
+    ],
+    base: '/react/',
 })
